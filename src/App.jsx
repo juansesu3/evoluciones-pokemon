44971d9abf86d2cc4d508a07b3b4a32bba86266b
Juan Sebastian Suarez Ramirez <90219339+juansesu3@users.noreply.github.com>
2023-02-19 08:46:18 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,104 @@
+// Compopnents
+import { Button } from "./components/Button";
+import { Card } from "./components/Card";
+// Styles
+import './sass/App.scss';
+// Icons
+import { TiArrowRightOutline, TiArrowLeftOutline } from "react-icons/ti";
+// Hooks
+import { useState, useEffect } from "react";
+
+
+const App = () => {
+
+    const [pokemonId, setPokemonId] = useState(1);
+    const [pokemonEvolutions, setPokemonEvolutions] = useState([]);
+
+    useEffect(() => {
+        getEvolutions(pokemonId);
+        console.log('useEffect executed')
+    }, [pokemonId])
+
+    async function getEvolutions(id) {
+        const response = await fetch(`https://pokeapi.co/api/v2/evolution-chain/${id}/`);
+        const data = await response.json();
+
+        let pokemonEvoArray = []
+
+        let pokemonLv1 = data.chain.species.name;
+        let pokemonLv1Img = await getPokemonImgs(pokemonLv1);
+        pokemonEvoArray.push([pokemonLv1, pokemonLv1Img]);
+
+        if (data.chain.evolves_to.length !== 0) {
+
+            let pokemonLv2 = data.chain.evolves_to[0].species.name;
+            let pokemonLv2Img = await getPokemonImgs(pokemonLv2);
+            pokemonEvoArray.push([pokemonLv2, pokemonLv2Img]);
+
+            console.log(pokemonEvoArray)
+            
+
+            if(data.chain.evolves_to[0].evolves_to.length!==0){
+                let pokemonLv3 = data.chain.evolves_to[0].evolves_to[0].species.name;
+                let pokemonLv3Img = await getPokemonImgs(pokemonLv3);
+                pokemonEvoArray.push([pokemonLv3, pokemonLv3Img]);
+                setPokemonEvolutions(pokemonEvoArray);
+                
+
+            }
+
+        }
+        setPokemonEvolutions(pokemonEvoArray);
+
+        // setPokemonName(data.chain.species.name)
+        //console.log(data.chain.species.name)
+    }
+
+    const getPokemonImgs = async (name) => {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}/`);
+        const data = await response.json();
+        return data.sprites.other['official-artwork'].front_default
+
+
+    }
+
+
+    const prevClick = () => {
+        (pokemonId === 1) ?
+            setPokemonId(1) :
+            setPokemonId(pokemonId - 1)
+
+    }
+
+
+
+    const nextClick = () => {
+
+        setPokemonId(pokemonId + 1)
+
+    }
+
+
+    return (
+        <div className="app">
+            {/* Tarjetas */}
+            <div className= {`card__container card${pokemonEvolutions.length}`}>
+            {pokemonEvolutions.map(pokemon =><Card key={pokemon[0]} name ={pokemon[0]} img ={pokemon[1]} />)}
+               
+            </div>
+            <div className="buttons-container">
+                <Button
+                    icon={<TiArrowLeftOutline />}
+                    handleClick={prevClick}
+                />
+                { }
+                <Button
+                    icon={<TiArrowRightOutline />}
+                    handleClick={nextClick}
+                />
+
+            </div>
+        </div>
+    )
+}
+export { App }
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
+import './sass/index.scss'
+
+const root = ReactDOM.createRoot(document.querySelector('#root'))
+
+
+root.render(<App/>)
+
